a690ef677b561237e75cab1a9a16ba75fe1848d1
Misja Alma <malma@xebia.com>
2022-12-03 07:52:58 +0100

    Day 3

---
+++ src/main/scala/aoc2022/Day3.scala
@@ -0,0 +1,34 @@
+package aoc2022
+
+def charScore(c: Char): Int =
+  if c > 'Z' then c - 'a' + 1 else c - 'A' + 1 + 26
+
+object Day3Part1 extends App {
+  val sc = scannerFromResource("/day3.txt")
+  val lines = scannerToLines(sc)
+
+  val sharedChars = lines.map { line =>
+    val trimmed = line.trim
+    val halfLength = trimmed.length/2
+    val half1 = trimmed.take(halfLength).toSet
+    val half2 = trimmed.drop(halfLength).toSet
+    val shared = half1.intersect(half2)
+    shared.head
+  }
+
+  val solutions = sharedChars.map(charScore)
+  println(solutions.sum)
+}
+
+object Day3Part2 extends App {
+  val sc = scannerFromResource("/day3.txt")
+  val lines = scannerToLines(sc)
+
+  val sharedChars = lines.sliding(3, 3).map { case p: Seq[String] =>
+    val shared = p.map(_.toSet).reduce(_ intersect _)
+    shared.head
+  }
+
+  val solutions = sharedChars.map(charScore)
+  println(solutions.sum)
+}
