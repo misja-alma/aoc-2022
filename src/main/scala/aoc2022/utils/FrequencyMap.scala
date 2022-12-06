7c56fd186b1d5e852a5100f2f66b369b8022b600
Misja Alma <misja.alma@gmail.com>
2022-12-06 16:50:48 +0100

    Use Scala 3 extension method

--- src/main/scala/aoc2022/utils/FrequencyMap.scala
+++ src/main/scala/aoc2022/utils/FrequencyMap.scala
@@ -4,7 +4,7 @@ type FrequencyMap[T] = Map[T, Long]
 
 def FrequencyMap[T](): FrequencyMap[T] = Map[T, Long]()
 
-implicit class FrequencyMapOps[T](map: FrequencyMap[T]) {
+extension[T](map: FrequencyMap[T]) {
   def addCount(el: T, count: Long): FrequencyMap[T] = {
     val newCount = count + map.getOrElse(el, 0L)
     map.updated(el, newCount)
