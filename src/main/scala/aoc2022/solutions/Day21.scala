44c942f48e8545a268564213d55ea902c68fe556
Misja Alma <misja.alma@gmail.com>
2022-12-21 09:15:53 +0100

    Cleanup

--- src/main/scala/aoc2022/solutions/Day21.scala
+++ src/main/scala/aoc2022/solutions/Day21.scala
@@ -6,35 +6,38 @@ object Day21 {
   val sc = scannerFromResource("/day21.txt")
   val lines = scannerToLines(sc)
 
-  val plus = """([a-z]+): ([a-z]+) \+ ([a-z]+)""".r
-  val multiply = """([a-z]+): ([a-z]+) \* ([a-z]+)""".r
-  val minus = """([a-z]+): ([a-z]+) \- ([a-z]+)""".r
-  val divide = """([a-z]+): ([a-z]+) \/ ([a-z]+)""".r
-  val value = """([a-z]+): (\d+)""".r
-
   enum Operation:
-    case Plus() extends Operation
-    case Minus() extends Operation
-    case Multiply() extends Operation
-    case Divide() extends Operation
+    case Plus extends Operation
+    case Minus extends Operation
+    case Multiply extends Operation
+    case Divide extends Operation
 
   import Operation.*
 
+  val operation = """([a-z]+): ([a-z]+) (\+|\*|\-|\/) ([a-z]+)""".r
+  val value = """([a-z]+): (\d+)""".r
+
+  def parseOperation(op: String): Operation =
+    op match {
+      case "+" => Plus
+      case "-" => Minus
+      case "/" => Divide
+      case "*" => Multiply
+      case _ => sys.error("Can't parse operaion " + op)
+    }
+
   def parseMonkey(op: String): Monkey = op match {
-    case minus(m, m1, m2) => Monkey(m, None, Seq(m1, m2), Some(Minus()))
-    case plus(m, m1, m2) => Monkey(m, None, Seq(m1, m2), Some(Plus()))
-    case divide(m, m1, m2) => Monkey(m, None, Seq(m1, m2), Some(Divide()))
-    case multiply(m, m1, m2) => Monkey(m, None, Seq(m1, m2), Some(Multiply()))
+    case operation(m, m1, op, m2) => Monkey(m, None, Seq(m1, m2), Some(parseOperation(op)))
     case value(m, value) => Monkey(m, Some(value.toInt), Seq(), None)
     case _ => sys.error("Cant parse: " + op)
   }
   
   def apply(op: Operation, value1: Long, value2: Long): Long =
     op match {
-      case Plus() => value1 + value2
-      case Minus() => value1 - value2
-      case Multiply() => value1 * value2
-      case Divide() => value1 / value2
+      case Plus => value1 + value2
+      case Minus => value1 - value2
+      case Multiply => value1 * value2
+      case Divide => value1 / value2
     }
 
   case class Monkey(name: String, var value: Option[Long], children: Seq[String], operation: Option[Operation])
@@ -67,23 +70,21 @@ object Day21 {
       }
     }
 
-
-
   def adjustDiffToHumnLeft(monkey: Monkey, existingDiff: Double): Double = {
     val childA = monkeyMap(monkey.children.head)
     val childB = monkeyMap(monkey.children.last)
     monkey.operation.get match {
-      case Plus() => existingDiff
-      case Minus() => existingDiff
+      case Plus => existingDiff
+      case Minus => existingDiff
       // (c * b) = (a * b) + y
       // c = ((a * b) + y) / b
       // newDiff = c - a
-      case Multiply() =>
+      case Multiply =>
         val c = (monkey.value.get + existingDiff) / childB.value.get
         c - childA.value.get
       // (c / b) = (a / b) + y
       // c = ((a / b) + y) * b
-      case Divide() =>
+      case Divide =>
         val c = (monkey.value.get + existingDiff) * childB.value.get
         c - childA.value.get
     }
@@ -93,14 +94,14 @@ object Day21 {
     val childA = monkeyMap(monkey.children.head)
     val childB = monkeyMap(monkey.children.last)
     monkey.operation.get match {
-      case Plus() => existingDiff
-      case Minus() => -existingDiff
-      case Multiply() =>
+      case Plus => existingDiff
+      case Minus => -existingDiff
+      case Multiply =>
         val c = (monkey.value.get + existingDiff) / childA.value.get
         c - childB.value.get
       // (a / c) = (a / b) + y
       // c = 1 / (((a / b) + y) / a) = a / ((a / b) + y)
-      case Divide() =>
+      case Divide =>
         val c = childA.value.get / (monkey.value.get + existingDiff)
         c - childB.value.get
     }
@@ -131,16 +132,14 @@ object Day21 {
 
   @main
   def day21Part2 = printSolution {
-    evaluate(rootMonkey)  // makes sure that all monkeys ahave a value
+    evaluate(rootMonkey)  // makes sure that all monkeys have a value
     val childA = monkeyMap(rootMonkey.children.head)
     val childB = monkeyMap(rootMonkey.children.last)
-    
-    //monkeyChildren.foreach(evaluate)
+
     // for each branch: make sure the humn branch equals the other branch
     // this means making it larger or smaller by some term
     
-    // humn is in branch left
-    val diff = childB.value.get - childA.value.get
+    val diff = if isHumnInBranch(childA) then childB.value.get - childA.value.get else childA.value.get - childB.value.get
     val solution = findValueForHumn(diff, childA)
     solution
   }    // 3378273370680
